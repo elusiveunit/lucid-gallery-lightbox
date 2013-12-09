5d8acc9cecead0bc2535ff9ef6b8438b985b961f
Jens Lindberg <elusiveunit@gmail.com>
2013-12-09 21:51:16 +0100

    __FILE__ symlink workaround, trac ticket #16953

--- lucid-gallery-lightbox.php
+++ lucid-gallery-lightbox.php
@@ -18,16 +18,23 @@
 // Block direct requests
 if ( ! defined( 'ABSPATH' ) ) die( 'Nope' );
 
+// Symlink workaround, see http://core.trac.wordpress.org/ticket/16953
+$lgljl_plugin_file = __FILE__;
+if ( isset( $plugin ) )
+	$lgljl_plugin_file = $plugin;
+elseif ( isset( $network_plugin ) )
+	$lgljl_plugin_file = $network_plugin;
+
 // Plugin constants
 if ( ! defined( 'LGLJL_VERSION' ) )
 	define( 'LGLJL_VERSION', '2.1.1' );
 
 if ( ! defined( 'LGLJL_PLUGIN_URL' ) )
-	define( 'LGLJL_PLUGIN_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+	define( 'LGLJL_PLUGIN_URL', trailingslashit( plugin_dir_url( $lgljl_plugin_file ) ) );
 
 if ( ! defined( 'LGLJL_PLUGIN_PATH' ) )
-	define( 'LGLJL_PLUGIN_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+	define( 'LGLJL_PLUGIN_PATH', trailingslashit( plugin_dir_path( $lgljl_plugin_file ) ) );
 
 // Load and initialize the plugin parts
 require LGLJL_PLUGIN_PATH . 'inc/core.php';
-$lucid_gallery_lightbox = new Lucid_Gallery_Lightbox( __FILE__ );
+$lucid_gallery_lightbox = new Lucid_Gallery_Lightbox( $lgljl_plugin_file );
